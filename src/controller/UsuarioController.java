8390b5b64b633cf310b4ce266650ba748e2eca95
Juani <87107074+VB6Enjoyer@users.noreply.github.com>
2021-10-29 21:54:12 -0300

    Bug en findby() arreglado

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -40,7 +40,7 @@ public class UsuarioController extends UsuarioDAOImp {
 	
 	@Override
 	public Usuario findBy(String campo, String operador, String valor) throws SQLException {
-		return super.findBy(campo, valor, operador);
+		return super.findBy(campo, operador, valor);
 	}
 
 	@Override
